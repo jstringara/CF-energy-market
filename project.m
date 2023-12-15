649c93ed568036a59ef0140a869bada680223d2b
jstringara <62561822+jstringara@users.noreply.github.com>
2023-12-15 20:08:17 +0100

    Monte Carlo simulation

--- project.m
+++ project.m
@@ -12,22 +12,46 @@ table_swaps = readtable(data_dir + "DataDEEEX.xlsx", 'Sheet', 'F');
 table_options = readtable(data_dir + "DataDEEEX.xlsx", 'Sheet', 'OptionsONQ42024');
 table_ois = readtable(data_dir + "DataDEEEX.xlsx", 'Sheet', 'ois');
 
+%% Discount Factors
+DF = table_ois(2, 1:end).Variables; % discount factors
+dates = table_ois(1, 1:end).Variables; % dates
+
+% take differences between the dates and today (04/11/2023)
+% we need to convert the dates to datenum format
+dates = dates - datenum('04/11/2023', 'dd/mm/yyyy');
+dates = dates / 365;
+
 %% Calibration
 
 F0 = table_swaps(end, end).Variables; % swap price at time 0
 K = table_options(1, 2:end).Variables; % strike prices
 T = table_options(2:end, 1).Variables; % maturities
-r = rate_ois(table_ois, T); % OIS rate
+r = 0.01 * ones(size(T)); % risk free rate
 
+%% Monte Carlo simulation with constant volatilities
+
+% Parameters
+N_sim = 10000; % number of simulations
+% Simulate the two brownian motions with N_sim paths and T time steps
+W1 = randn(N_sim, length(T)-1);
+W2 = randn(N_sim, length(T)-1);
+
+% Change the variance at each time step to be Tn - Tn-1
+% Each W ~ N(0,1), so we make it N(0, Tn - Tn-1)
+for i = 2:length(T)
+    W1(:, i) = W1(:, i) * sqrt(T(i) - T(i-1));
+    W2(:, i) = W2(:, i) * sqrt(T(i) - T(i-1));
+end
 
 market_prices = table_options(2:end, 2:end).Variables; % real market prices
+
+% error function is the mse between the market prices and the model prices
+func = @(x) mean(mean( (market_prices - price_swaption_MC(F0, K, T, r, x(1), x(2), N_sim, W1, W2)).^2));
 
 % Calibration of the model
 % we use fmincon to minimize the error between the market prices and the
 % model prices
 
-% error function is the mse between the market prices and the model prices
-func = @(x) mean(mean( (market_prices - price_swaption_batch(F0, K, T, r, x(1), x(2))).^2));
 % initial guess, random number between 0.01 and 1
 x0 = rand(1, 2) * 0.99 + 0.01;
 
@@ -38,9 +62,14 @@ lb = [0.01, 0.01];
 
 %% Plot the calibration
 
+% compute the model prices
+
+model_prices = price_swaption_MC(F0, K, T, r, x(1), x(2), N_sim, W1, W2);
+
 % Plot the market prices
 figure; 
 hold on
+% plot the market prices using only shades of red
 surf(K, T, market_prices);
 xlabel('Strike price');
 ylabel('Maturity');
@@ -48,7 +77,7 @@ zlabel('Market price');
 title('Market prices of swaptions');
 
 % Plot the model prices
-surf(K, T, price_swaption_batch(F0, K, T, r, x(1), x(2)))
+surf(K, T, model_prices);
 zlabel('Model price')
 
 legend('Market price', 'Model price')
