400b0989625408160696ff8d656394be05c6544d
jstringara <62561822+jstringara@users.noreply.github.com>
2023-12-16 17:07:42 +0100

    Model with two time dependent volatilities

--- timeDependentVolatilitySwaption.m
+++ timeDependentVolatilitySwaption.m
@@ -1,17 +1,18 @@
-function C = timeDependentVolatilitySwaption(sigma_1, alpha, sigma_2, F0, K, T, DF, T1)
+function C = timeDependentVolatilitySwaption(sigma_1, alpha, sigma_2, beta, F0, K, T, DF, T1, T2)
     % Price a swaption with time-dependent volatility using the Black 76 formula
     % the model is of the form:
     %   dF/F = sigma_1(t) dW_1 + sigma_2 dW_2
     % where W_1 and W_2 are independent Brownian motions
-    %   TODO: write the formula
-    %   sigma_1(T_1, t) = sigma_1 * exp(-alpha *(T_1 - t))
-    %   sigma_2 = constant
+    %   sigma_1(t, T1, T2) =
+    %       sigma_1 / (alpha * (T2-T2)) * [ exp(-alpha * (T1-t)) - exp(-alpha * (T2-t))]
+    %   sigma_2(t, T1, T2) =
+    %       sigma_2 / (beta * (T2-T2)) * [ exp(-beta * (T1-t)) - exp(-beta * (T2-t))]
     % 
     % Inputs:
-    %   sigma_1 and alpha: parameters of the time-dependent volatility
+    %   sigma_1 and alpha: parameters of the first time-dependent volatility
+    %   sigma_2 and beta: parameters of the second time-independent volatility
     %   T_1: the beginning of the swap 
     %   T_2: the end of the swap
-    %   sigma_2: the constant volatility
     %   F0: the price of the swap at the time of pricing
     %   K: vector of strike prices
     %   T: vector of maturities
@@ -22,8 +23,12 @@ function C = timeDependentVolatilitySwaption(sigma_1, alpha, sigma_2, F0, K, T, 
     %
 
     % compute the integral of the squared volatility for each maturity
-    sigma_1_int = sigma_1^2 / (2 * alpha) * exp(-2 * alpha * T1) * (exp(2 * alpha * T) - 1);
-    sigma_2_int = sigma_2^2 * T;
+    sigma_1_int = sigma_1^2 / (2 * alpha^3 * (T2-T1)^2) * ...
+        ( exp(-alpha*T1) - exp(-alpha*T2) )^2 * ...
+        ( exp(2*alpha*T) - 1); % exp(2*alpha*t) = 1 since t = 0
+    sigma_2_int = sigma_2^2 / (2 * beta^3 * (T2-T1)^2) * ...
+        ( exp(-beta*T1) - exp(-beta*T2) )^2 * ...
+        ( exp(2*beta*T) - 1); % exp(2*beta*t) = 1 since t = 0
     sigma_hat = sqrt(sigma_1_int + sigma_2_int);
 
     % compute the price of the swaption for each maturity and strike
