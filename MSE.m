2e649c54fa05e58a2ceef46d6013c220533034d5
jstringara <62561822+jstringara@users.noreply.github.com>
2023-12-16 17:05:31 +0100

    Refactor MSE into its own function

---
+++ MSE.m
@@ -0,0 +1,18 @@
+function MSE = MSE(y, y_hat)
+    % Calculate the mean squared error between the two matrices y and y_hat of the same size
+    %
+    % Inputs:
+    %   y: the true values
+    %   y_hat: the predicted values
+    %
+    % Output:
+    %   MSE: the mean squared error between the two matrices y and y_hat
+
+    % check if the two matrices have the same size
+    if size(y) ~= size(y_hat)
+        error('The two matrices must have the same size');
+    end
+
+    % calculate the mean squared error
+    MSE = mean(mean((y - y_hat).^2), "all");
+end
