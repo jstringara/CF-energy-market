0be7862a4b9863df120c382d188350dbbf5e8098
jstringara <62561822+jstringara@users.noreply.github.com>
2023-12-26 17:56:01 +0100

    Corrected to have one deterministic and one constant

--- deterministicVolatilitySwaption.m
+++ deterministicVolatilitySwaption.m
@@ -1,5 +1,5 @@
 function [C, sigma_hat] = deterministicVolatilitySwaption( ...
-    sigma_1, alpha_1, sigma_2, alpha_2, T1, T2, F0, K, T, DF)
+    sigma_1, alpha_1, sigma_2, T1, T2, F0, K, T, DF)
     % Price a swaption with time-dependent volatility using the Black 76 formula
     % the model is of the form:
     %   dF/F = sigma_1(t) dW_1 + sigma_2 dW_2
@@ -27,7 +27,6 @@ function [C, sigma_hat] = deterministicVolatilitySwaption( ...
         sigma_1 double {mustBePositive}
         alpha_1 double {mustBePositive}
         sigma_2 double {mustBePositive}
-        alpha_2 double {mustBePositive}
         T1 double {mustBePositive}
         T2 double {mustBePositive}
         F0 double
@@ -41,8 +40,7 @@ function [C, sigma_hat] = deterministicVolatilitySwaption( ...
     % compute the integral of the squared volatility for each maturity
     sigma_1_int = sigma_1^2 / (alpha_1^3 * (T2-T1)^2) * (exp(-alpha_1 * T1) - exp(-alpha_1 * T2)^2) ...
         * (exp(2 * alpha_1 * T) - 1);
-    sigma_2_int = sigma_2^2 / (alpha_2^3 * (T2-T1)^2) * (exp(-alpha_2 * T1) - exp(-alpha_2 * T2)^2) ...
-        * (exp(2 * alpha_2 * T) - 1);
+    sigma_2_int = sigma_2^2 .* T; % constant volatility
     sigma_hat = sqrt(sigma_1_int + sigma_2_int);
 
     % compute the price of the swaption for each maturity and strike
